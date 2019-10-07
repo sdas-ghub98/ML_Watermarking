4854bab048e98636ece0544f12bae5bc84a08a45
Sourish Das <sourishd821@gmail.com>
2019-10-07 21:23:00 +0530

    Complete logic for the code

--- Codes/Embedding_Algo.py
+++ Codes/Embedding_Algo.py
@@ -1,7 +1,8 @@
 import cv2
 import random
-import os
 import pywt
+import numpy as np
+from PIL import Image
 
 location = "F:/NIIT University/4 Year/Machine Learning/Term Project/Dataset/"
 #frames = []
@@ -47,13 +48,47 @@ def RGB_Splitter(rf):
     return red, green, blue
 
 def ApplyDWT(r,g,b):
-    cAR,cDR = pywt.dwt(r,'db1')
-    cAG,cDG = pywt.dwt(g,'db1')
-    cAB,cDB = pywt.dwt(b,'db1')
-    return cAR, cDR, cAG,cDG,cAB,cDB
+    [LLR1,HHR1] = pywt.dwt(r,'db1')
+    [LLG1,HHG2] = pywt.dwt(g,'db1')
+    [LLB1,HHB2] = pywt.dwt(b,'db1')
 
     #Take the HH sub band and apply DWT again here
+    [LLR2,HHR2] = pywt.dwt(LLR1,'db1')
+    [LLG2,HHG2] = pywt.dwt(LLG1,'db1')
+    [LLB2,HHB2] = pywt.dwt(LLB1,'db1')
 
-#def ApplySVD(hR, hG, hB):
+    return HHR2,HHG2,HHB2
 
-    #Take the result of this and apply SVD there
+def ApplySVD(hr,hg,hb):
+    ur, sr, vhr = np.linalg.svd(hr, full_matrices=False)
+    ug, sg, vhg = np.linalg.svd(hg, full_matrices=False)
+    ub, sb, vhb = np.linalg.svd(hb, full_matrices=False)
+    return ur,sr,vhr,ug,sg,vhg,ub,sb,vhb
+
+
+def RGB_Merger(c):
+    rgbArray = np.zeros((512,512,3), 'uint8')
+    rgbArray[..., 0] = (c[0]+c[1]+c[2])*256
+    rgbArray[..., 1] = (c[3]+c[4]+c[5])*256
+    rgbArray[..., 2] = (c[6]+c[7]+c[8])*256
+    img = Image.fromarray(rgbArray,'RGB')
+    img.save(location + 'cf.png')
+
+def FrameConstruction(nof):
+    
+    p1 = location + 'cf.png'
+    p2 = location + 'lg.png'
+    src1 = cv2.imread(p1)
+    src2 = cv2.imread(p2)
+    im3 = src1 + src2
+    cv2.imwrite(location+'wmk_frame.png',im3)
+
+    for i in range(0, nof, 1):
+        im_location = location + 'Sub_Frames/sub'+str(i) +'.png'
+        src3 = cv2.imread(im_location)
+        src3 = im3 + src3
+        cv2.imwrite(location + "Sub_Frames/sub%d.png" % i, src3)
+    
+    return im3
+
+
--- Codes/Main.py
+++ Codes/Main.py
@@ -2,15 +2,51 @@ import Embedding_Algo as cea
 import Logo as logo
 if __name__ == '__main__': 
     
-    #Embedding algorithm
-    nof = cea.FrameCapture(cea.location + "Akiyo Video.mp4")
-    random_frame = cea.FrameSubtraction(nof)
-    r1, g1, b1 = cea.RGB_Splitter(random_frame)
-    a1,b1,c1,d1,e1,f1 = cea.ApplyDWT(r1,g1,b1)
+    #Calling the embedding algorithm one by one
+
+    #Splitting the video frames
+    #nof = cea.FrameCapture(cea.location + "Akiyo Video.mp4")
     
-    #cea.ApplySVD(r1,g1,b1)
+    #Taking a random frame and subtracting it from the remaining frames
+    #random_frame = cea.FrameSubtraction(nof)
+
+    #Splitting the random frame into RGB
+    r1, g1, b1 = cea.RGB_Splitter(131)
+    
+    #Applying DWT twice on the splitted random frame
+    a1,b1,c1 = cea.ApplyDWT(r1,g1,b1)
+
+    #Applying SVD on the HH band of the random frame
+    ur,sr,vhr,ug,sg,vhg,ub,sb,vhb = cea.ApplySVD(r1,g1,b1)
+    frames= []
+    frames.extend((ur,sr,vhr,ug,sg,vhg,ub,sb,vhb))
+
+    print("Frames processing done")
+    
+    #Processing the logo by first doing the RGB split
     r2,g2,b2 = logo.RGB_Splitter()
-    a2,b2,c2,d2,e2,f2 = logo.ApplyDWT_Logo(r2,g2,b2)
-    #logo.ApplySVD_Logo
+
+    #Applying DWT once on the splitted logo
+    a2,b2,c2 = logo.ApplyDWT_Logo(r2,g2,b2)
+
+    #Applying SVD once on the DWT-ized logo
+    url,srl,vhrl,ugl,sgl,vhgl,ubl,sbl,vhbl = logo.ApplySVD_Logo(a2,b2,c2)
+    logo_frame = []
+    logo_frame.extend((url,srl,vhrl,ugl,sgl,vhgl,ubl,sbl,vhbl))
+    
+    print("Logo processing successful")
+
+
+    #Adding the SVD frame and SVD logo together
+    print("Putting the logo into the video frame...")
+    #Converting the video frame to RGB 
+    cea.RGB_Merger(frames)
+    logo.RGB_Merger_Logo(logo_frame)
+    
+    # Once we get two separate RGB image saved, let us now add them together and then add the result to all the subtracted frames.
+    wmk_frame = cea.FrameConstruction(300)
+
+    #Constructing the video from these frames
+    #cea.CreateVideo(wmk_frame)
 
     #Extracting algorithm
