0bdfc507efae2afb8d568e5c957a6beaf7528ed4
Sourish Das <sourishd821@gmail.com>
2019-10-12 11:25:47 +0530

    Modified code

--- Codes/Embedding_Algo.py
+++ Codes/Embedding_Algo.py
@@ -1,94 +1,100 @@
 import cv2
 import random
 import pywt
-import numpy as np
+from numpy import diag,dot
 from PIL import Image
+from scipy.linalg import svd
 
 location = "F:/NIIT University/4 Year/Machine Learning/Term Project/Dataset/"
-#frames = []
-#gray_frames = []
-#sub_frames = []
+location2 = "F:/NIIT University/4 Year/Machine Learning/Term Project/Dataset/Logo.png"
+frames = []
+red_frames = []
+green_frames = []
+blue_frames = []
+
 # Function to extract frames 
 def FrameCapture(path): 
     cap = cv2.VideoCapture(path) 
+    success, image = cap.read()
     count = 0
     success = 1
-
     while success:
         success, image = cap.read()
-        #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(location + "Frames/image%d.png"%count,image)
-        #frames.append(image)
-        count += 1    
-    print("Video was split into frames successfully")
-    #print(gray_image)
+        if success:
+            red = image[:,:,2]
+            green = image[:,:,1]
+            blue = image[:,:,0]    
+            red_frames.append(red)
+            green_frames.append(green)
+            blue_frames.append(blue)
+        count += 1
+    
+    for i in range(0,count,1):
+        image = cv2.imread(location+'Frames/image'+str(count)+'.png')
+        
+    print("--------------Video was split into frames successfully and splitted into RGB frames --------------")
     return count
 
-def FrameSubtraction(nof):
-    d = random.randrange(0,nof,1)
-    rnd_frame_path = location+"Frames/image"+str(d)+".png"
-    src1 = cv2.imread(rnd_frame_path)
-    for i in range(0,nof,1):
-        path2 = location+"Frames/image"+str(i)+".png" 
-        src2 = cv2.imread(path2)
-        im3 = src1-src2
-        cv2.imwrite(location + "Sub_Frames/sub%d.png" % i, im3)
-        #im3.delete()
-    #print(sub_frames)
-    return d
+#Perfprming frame subtraction by selecting random frame from each channel and subtracting it channel wise
+def Frame_Subtract(nof):
+    sub_red_frames = []
+    sub_green_frames = []
+    sub_blue_frames = []
+    dr = random.choice(red_frames)
+    dg = random.choice(green_frames)
+    db = random.choice(blue_frames)
+    sub_red_frames.append(red_frames- dr)
+    sub_green_frames.append(green_frames-dg)
+    sub_blue_frames.append(blue_frames-db)
+    print("-------------- Random frames from each channel were selected and subtracted accordingly --------------")
+    return sub_red_frames,sub_green_frames,sub_blue_frames,dr,dg,db
 
-def RGB_Splitter(rf):
-    src = cv2.imread(location + "Frames/image"+str(rf)+".png")
+def ApplyDWT_Frames(r,g,b):
+    LLR1,_ = pywt.dwt(r,'db1')
+    LLG1,_ = pywt.dwt(g,'db1')
+    LLB1,_ = pywt.dwt(b,'db1')
+
+    #Take the HH sub band and apply DWT again here
+    _,HHR2 = pywt.dwt(LLR1,'db1')
+    _,HHG2 = pywt.dwt(LLG1,'db1')
+    _,HHB2 = pywt.dwt(LLB1,'db1')
+
+    print("-------------- DWT applied twice once on normal and then on LL sub band. Returning the HH sub bands --------------")
+    return HHR2,HHG2,HHB2
+
+def RGB_Splitter_Logo():
+    src = cv2.imread(location2)
     red = src[:,:,2]
     green = src[:,:,1]
     blue = src[:,:,0]
-    print(red)
-    print(green)
-    print(blue)
+    print("-------------- Logo splitted successfully --------------")
     return red, green, blue
 
-def ApplyDWT(r,g,b):
-    [LLR1,HHR1] = pywt.dwt(r,'db1')
-    [LLG1,HHG2] = pywt.dwt(g,'db1')
-    [LLB1,HHB2] = pywt.dwt(b,'db1')
-
-    #Take the HH sub band and apply DWT again here
-    [LLR2,HHR2] = pywt.dwt(LLR1,'db1')
-    [LLG2,HHG2] = pywt.dwt(LLG1,'db1')
-    [LLB2,HHB2] = pywt.dwt(LLB1,'db1')
-
-    return HHR2,HHG2,HHB2
+def ApplyDWT_Logo(r,g,b):
+    _, HHR = pywt.dwt(r,'db1')
+    _, HHG = pywt.dwt(g,'db1')
+    _, HHB = pywt.dwt(b,'db1') 
+    print("-------------- DWT applied on Logo --------------")
+    return HHR,HHG,HHB
 
 def ApplySVD(hr,hg,hb):
-    ur, sr, vhr = np.linalg.svd(hr, full_matrices=False)
-    ug, sg, vhg = np.linalg.svd(hg, full_matrices=False)
-    ub, sb, vhb = np.linalg.svd(hb, full_matrices=False)
-    return ur,sr,vhr,ug,sg,vhg,ub,sb,vhb
+    Ur, sr, VTr = svd(hr)
+    Ug, sg, VTg = svd(hg)
+    Ub, sb, VTb = svd(hb)
+    print("-------------- Applying SVD on the HH sub bands successful --------------")
+    return Ur,sr,VTr,Ug,sg,VTg,Ub,sb,VTb
 
+def InverseSVD(u,s,vt):
+    print(u.shape)
+    print(s.shape)
+    print(vt.shape)
+    '''Sigma = diag(s)
+    B = u.dot(Sigma.dot(vt))
+    print("-------------- Inverse SVD applied --------------")
+    return B'''
 
-def RGB_Merger(c):
-    rgbArray = np.zeros((512,512,3), 'uint8')
-    rgbArray[..., 0] = (c[0]+c[1]+c[2])*256
-    rgbArray[..., 1] = (c[3]+c[4]+c[5])*256
-    rgbArray[..., 2] = (c[6]+c[7]+c[8])*256
-    img = Image.fromarray(rgbArray,'RGB')
-    img.save(location + 'cf.png')
+#def IDWT(mat):
 
-def FrameConstruction(nof):
     
-    p1 = location + 'cf.png'
-    p2 = location + 'lg.png'
-    src1 = cv2.imread(p1)
-    src2 = cv2.imread(p2)
-    im3 = src1 + src2
-    cv2.imwrite(location+'wmk_frame.png',im3)
-
-    for i in range(0, nof, 1):
-        im_location = location + 'Sub_Frames/sub'+str(i) +'.png'
-        src3 = cv2.imread(im_location)
-        src3 = im3 + src3
-        cv2.imwrite(location + "Sub_Frames/sub%d.png" % i, src3)
-    
-    return im3
 
 
--- Codes/Main.py
+++ Codes/Main.py
@@ -1,52 +1,66 @@
-import Embedding_Algo as cea
-import Logo as logo
+import Embedding_Algo as ea
+import numpy as np
+
 if __name__ == '__main__': 
     
-    #Calling the embedding algorithm one by one
+    #EMBEDDING ALGORITHM
 
-    #Splitting the video frames
-    #nof = cea.FrameCapture(cea.location + "Akiyo Video.mp4")
+    #Splitting the video frames and then splitting them on RGB frames
+    nof = ea.FrameCapture(ea.location + "Akiyo Video.mp4")
     
-    #Taking a random frame and subtracting it from the remaining frames
-    #random_frame = cea.FrameSubtraction(nof)
+    #Performing frame subtraction on all the channeas
+    #Receiving the sub frames matrices and random frames from R,G and B channea
+    sr,sg,sb,rr,rg,rb = ea.Frame_Subtract(nof)
 
-    #Splitting the random frame into RGB
-    r1, g1, b1 = cea.RGB_Splitter(131)
-    
-    #Applying DWT twice on the splitted random frame
-    a1,b1,c1 = cea.ApplyDWT(r1,g1,b1)
+    #Applying two rounds of DWT on the random frame
+    a1,b1,c1 = ea.ApplyDWT_Frames(rr,rg,rb)
 
-    #Applying SVD on the HH band of the random frame
-    ur,sr,vhr,ug,sg,vhg,ub,sb,vhb = cea.ApplySVD(r1,g1,b1)
-    frames= []
-    frames.extend((ur,sr,vhr,ug,sg,vhg,ub,sb,vhb))
+    #Applying SVD on the random frame
+    u1,s1,vt1,u2,s2,vt2,u3,s3,vt3 = ea.ApplySVD(a1,b1,c1)
+    s1 = np.pad(s1,(18),mode='constant')
+    s2 = np.pad(s2,(18),mode='constant')
+    s3 = np.pad(s3,(18),mode='constant')
+    vt1 = np.pad(vt1,(18,18),mode='constant')
+    vt2 = np.pad(vt2,(18,18),mode='constant')
+    vt3 = np.pad(vt3,(18,18),mode='constant')
 
-    print("Frames processing done")
-    
     #Processing the logo by first doing the RGB split
-    r2,g2,b2 = logo.RGB_Splitter()
+    r2,g2,b2 = ea.RGB_Splitter_Logo()
 
-    #Applying DWT once on the splitted logo
-    a2,b2,c2 = logo.ApplyDWT_Logo(r2,g2,b2)
-
-    #Applying SVD once on the DWT-ized logo
-    url,srl,vhrl,ugl,sgl,vhgl,ubl,sbl,vhbl = logo.ApplySVD_Logo(a2,b2,c2)
-    logo_frame = []
-    logo_frame.extend((url,srl,vhrl,ugl,sgl,vhgl,ubl,sbl,vhbl))
+    #Applying DWT once on the splitted logo and acquiring the HH sub band
+    a2,b2,c2 = ea.ApplyDWT_Logo(r2,g2,b2)
     
-    print("Logo processing successful")
-
-
-    #Adding the SVD frame and SVD logo together
-    print("Putting the logo into the video frame...")
-    #Converting the video frame to RGB 
-    cea.RGB_Merger(frames)
-    logo.RGB_Merger_Logo(logo_frame)
+    #Applying SVD once on the HH sub DWT-ized logo
+    u4,s4,vt4,u5,s5,vt5,u6,s6,vt6, = ea.ApplySVD(a2,b2,c2)
+    u4 = np.pad(u4,(9,9),mode='constant')
+    u5 = np.pad(u5,(9,9),mode='constant')
+    u6 = np.pad(u6,(9,9),mode='constant')
+    s4 = np.zeros((124,))
+    s5 = np.zeros((124,))
+    s6 = np.zeros((124,))
+    vt4 = np.zeros((124,))
+    vt5 = np.zeros((124,))
+    vt6 = np.zeros((124,))
+    #Now adding the SVD matrices
     
-    # Once we get two separate RGB image saved, let us now add them together and then add the result to all the subtracted frames.
-    wmk_frame = cea.FrameConstruction(300)
+    #For Red Frame
+    u7 = u1 + u4
+    s7 = s1 + s4
+    vt7 = vt1 + vt4
 
-    #Constructing the video from these frames
-    #cea.CreateVideo(wmk_frame)
+    u8 = u2 + u5
+    s8 = s2 + s5
+    vt8 = vt2 + vt5
 
-    #Extracting algorithm
+    u9 = u3 + u6
+    s9 = s3 + s6
+    vt9 = vt3 + vt6 
+
+    print("-------------- Sums done! --------------")
+
+    #Reconstructing three SVD matrixes for R,G and B channels separately
+    d1 = ea.InverseSVD(u7,s7,vt7)
+    d2 = ea.InverseSVD(u8,s8,vt8)
+    d3 = ea.InverseSVD(u9,s9,vt9)
+
+    #Applying inverse DWT to the reconstructed frames'''
